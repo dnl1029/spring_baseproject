94d10399729f1a3be986ab158a0374a0f1f29b40
kyu <dnl1029@gmail.com>
2023-02-18 13:27:34 +0900

    20230218 springcore commit

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,7 +3,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인
     @Override
